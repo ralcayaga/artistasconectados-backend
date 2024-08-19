85031733845ed672aec88569627450cd3a08940a
ralcayag <ralcayaga74@gmail.com>
2024-08-19 14:07:57 -0400

    Modelo creado

---
+++ src/models/artistas.ts
@@ -0,0 +1,16 @@
+import { Comentario } from 'src/models/comentarios';
+
+export class Artista {
+  constructor(
+        public id: number,                  //identificador único de la obra, generado automáticamente
+        public ruta: string,                //ruta al archivo de imagen o video de la obra
+        public descripcion: string,         //descripción de la obra
+        public etiquetas: string[],         //lista de etiquetas asociadas a la obra
+        public fechaSubida: Date,           //fecha en la que se subió la obra
+        public artista: Artista,            //artista que subió la obra
+        public meGusta: Artista[],          //lista de artistas que le han dado "me gusta" a la obra
+        public comentarios: Comentario[]    //lista de comentarios asociados a la obra
+
+
+  ) {}
+}
---
+++ src/models/comentarios.ts
@@ -0,0 +1,11 @@
+import { Artista } from 'src/models/artistas';
+
+export class Comentario {   
+  constructor(
+   public id: number,           //identificador único del comentario, generado automáticamente
+   public texto: string,        //texto del comentario
+   public fecha: Date,          //fecha y hora en la que se escribió el comentario
+   public artista: Artista      //artista que escribió el comentario
+    
+  ) {}
+}
